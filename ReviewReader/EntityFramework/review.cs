30a5bd38924d28f6e711368b64fa582b80c3a5bf
Jace patel <jace.patel@gmail.com>
2014-10-10 17:36:33 +1000

    More files to inital
    add

---
+++ ReviewReader/EntityFramework/review.cs
@@ -0,0 +1,39 @@
+namespace TextReader.EntityFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("inb302.reviews")]
+    public partial class review
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        [Required]
+        [StringLength(4000)]
+        public string ItemName { get; set; }
+
+        public int ReviewersOfReview { get; set; }
+
+        public int ReviewersOfReviewFoundHelpful { get; set; }
+
+        public int StarsGiven { get; set; }
+
+        [StringLength(500)]
+        public string ShortReview { get; set; }
+
+        [StringLength(100)]
+        public string ReviewerId { get; set; }
+
+        [StringLength(100)]
+        public string ReviewLocation { get; set; }
+
+        [StringLength(4000)]
+        public string LongReview { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool? IsAmazonVerifiedPurchase { get; set; }
+    }
+}
